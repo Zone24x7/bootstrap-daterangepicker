ac6e970115ddf49a5c7a9e15691c5b5adea9a1c9
Sasitha Gunadasa <sasithag@zone24x7.com>
2016-03-16 11:38:52 +0530

    updated package.js file

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.1.19',
+  version: '2.1.20',
   summary: 'Date range picker component for Bootstrap',
-  git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
+  git: 'https://github.com/Zone24x7/bootstrap-daterangepicker/',
   documentation: 'README.md'
 });
 
